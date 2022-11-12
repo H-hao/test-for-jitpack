a8e4a260a7adddde4943054ad8fa7448eceaac26
H-hao <z211063515@outlook.com>
2022-11-12 23:53:24 +0800

    Add files via upload

---
+++ src/main/java/com/example/testforjitpack/TestForJitpackApplication.java
@@ -0,0 +1,13 @@
+package com.example.testforjitpack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestForJitpackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestForJitpackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/testforjitpack/TestForJitpackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testforjitpack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestForJitpackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
